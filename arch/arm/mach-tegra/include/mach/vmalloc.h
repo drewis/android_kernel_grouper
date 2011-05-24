JonnyVR1/android_kernel_common

HACK: [ARM] tegra: reduce vmalloc space for nvmap
Signed-off-by: Erik Gilling <konkers@android.com>

arch/arm/mach-tegra/include/mach/vmalloc.h
@@ -23,6 +23,6 @@
 
 #include <asm/sizes.h>
 
-#define VMALLOC_END        0xFE000000
+#define VMALLOC_END        0xF8000000
 
 #endif

drewis/android_kernel_grouper

arch/arm/mach-tegra/include/mach/vmalloc.h
@@ -23,6 +23,6 @@
 
 #include <asm/sizes.h>
 
-#define VMALLOC_END        0xFE000000UL
+#define VMALLOC_END        0xF8000000UL
 
 #endif