1539b894fec1d4824ecafd8508a86e76bcc57662
Benoit Goby <benoit@android.com>
2011-07-14 20:48:20 -0700

    usb: serial: mdm6600: Only resume USB outside of suspend
    When the kernel is in the process of suspending, there's
    a race condition where usb could come up, ACK a packet
    of data from an attached device, and then suspend again
    with no consumer of data reading the data and that data
    is lost.
    
    Signed-off-by: Nathan Connell <w14185@motorola.com>
    
    Conflicts:
    
    	drivers/usb/serial/mdm6600.c

--- drivers/usb/serial/mdm6600.c
+++ drivers/usb/serial/mdm6600.c
@@ -125,6 +125,16 @@ static void mdm6600_wake_work(struct work_struct *work)
 
 	dbg("%s: port %d", __func__, modem->number);
 
+	/* Don't proceed during device state transitions. */
+	/* FIXME: Is this still needed:
+	if (intf->dev.power.status < DPM_OFF &&
+	    intf->dev.power.status != DPM_ON) {
+		if (!wait_for_completion_timeout(&intf->dev.power.completion,
+		                                                          HZ))
+			pr_err("%s: wait timed out", __func__);
+	}
+	*/
+
 	device_lock(&intf->dev);
 
 	if (intf->dev.power.in_suspend) {
