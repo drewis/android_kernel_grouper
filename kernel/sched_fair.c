a388981be5df7d93e9ba73fd5daefe2d64b178ee
Peter Zijlstra <peterz@infradead.org>
2011-12-15 11:47:00 -0800

    sched: Fix select_idle_sibling() regression in selecting an idle SMT sibling
    Mike Galbraith reported that this recent commit:
    
       commit 4dcfe1025b513c2c1da5bf5586adb0e80148f612
       Author: Peter Zijlstra <peterz@infradead.org>
       Date:   Thu Nov 10 13:01:10 2011 +0100
    
           sched: Avoid SMT siblings in select_idle_sibling() if possible
    
    stopped selecting an idle SMT sibling when there are no idle
    cores in a single socket system.
    
    Intent of the select_idle_sibling() was to fallback to an idle
    SMT sibling, if it fails to identify an idle core. But this
    fallback was not happening on systems where all the scheduler
    domains had `SD_SHARE_PKG_RESOURCES' flag set.
    
    Fix it. Slightly bigger patch of cleaning all these goto's etc
    is queued up for the next release.
    
    Reported-by: Mike Galbraith <efault@gmx.de>
    Reported-by: Alex Shi <alex.shi@intel.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Link: http://lkml.kernel.org/r/1323978421.1984.244.camel@sbsiddha-desk.sc.intel.com
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -2343,13 +2343,11 @@ again:
 		if (!smt && (sd->flags & SD_SHARE_CPUPOWER))
 			continue;
 
-		if (!(sd->flags & SD_SHARE_PKG_RESOURCES)) {
-			if (!smt) {
-				smt = 1;
-				goto again;
-			}
+		if (smt && !(sd->flags & SD_SHARE_CPUPOWER))
 			break;
-		}
+
+		if (!(sd->flags & SD_SHARE_PKG_RESOURCES))
+			break;
 
 		sg = sd->groups;
 		do {
@@ -2368,6 +2366,10 @@ again:
 next:
 			sg = sg->next;
 		} while (sg != sd->groups);
+	}
+	if (!smt) {
+		smt = 1;
+		goto again;
 	}
 done:
 	rcu_read_unlock();
