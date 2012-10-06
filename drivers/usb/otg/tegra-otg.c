fca2d1fe779665edc30b62219541dec29a213cdd
faux123 <reioux@gmail.com>
2012-10-05 22:08:34 -0700

    tegra-otg: HACK to allow OTG peripheral and charging at the same time
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/usb/otg/tegra-otg.c
+++ drivers/usb/otg/tegra-otg.c
@@ -64,6 +64,11 @@ struct tegra_otg_data {
 };
 static struct tegra_otg_data *tegra_clone;
 
+#ifdef CONFIG_USB_OTG_ON_CHARGING
+static bool tegra_otg_on_charging = false;
+module_param(tegra_otg_on_charging, bool, 0664);
+#endif
+
 static inline unsigned long otg_readl(struct tegra_otg_data *tegra,
 				      unsigned int offset)
 {
@@ -231,11 +236,13 @@ static void irq_work(struct work_struct *work)
 					      tegra_state_name(to));
 
 		if (tegra->charger_cb) {
-		  // tmtmtm: disable charging of OTG slave
-			//tegra->charger_cb(to, from, tegra->charger_cb_data);
-			// tmtmtm: enable vbus in
-  		tegra->detect_vbus = true;
-	  }
+			if (tegra_otg_on_charging)
+				/* enable v_bus detection for charging */
+				tegra->detect_vbus = true;
+			else
+				/* enable OTG to supply internal power */
+				tegra->charger_cb(to, from, tegra->charger_cb_data);				
+		}
 
 		if (to == OTG_STATE_A_SUSPEND) {
 			if (from == OTG_STATE_A_HOST)
