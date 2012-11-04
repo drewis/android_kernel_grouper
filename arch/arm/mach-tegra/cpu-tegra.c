32879d54dadfd68654f06ac43ea517dbf93c8fda
Metallice <BryanUofD@gmail.com>
2012-11-04 17:44:08 -0500

    cpu-tegra.c: remove stress test code to allow building without userspace gov

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -493,7 +493,7 @@ unsigned int tegra_getspeed(unsigned int cpu)
 	rate = clk_get_rate(cpu_clk) / 1000;
 	return rate;
 }
-extern bool stress_test_enable;
+//extern bool stress_test_enable;
 int tegra_update_cpu_speed(unsigned long rate)
 {
 	int ret = 0;
@@ -531,10 +531,10 @@ int tegra_update_cpu_speed(unsigned long rate)
 	for_each_online_cpu(freqs.cpu)
 		cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
 
-	if(stress_test_enable)
-		printk(KERN_DEBUG "cpufreq-tegra: transition: %u --> %u\n",
-			freqs.old, freqs.new);
-
+//	if(stress_test_enable)
+//		printk(KERN_DEBUG "cpufreq-tegra: transition: %u --> %u\n",
+//			freqs.old, freqs.new);
+//
 	ret = clk_set_rate(cpu_clk, freqs.new * 1000);
 	if (ret) {
 		pr_err("cpu-tegra: Failed to set cpu frequency to %d kHz\n",
