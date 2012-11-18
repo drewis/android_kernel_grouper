a1a2bc68a936139f54e97fd581149c47f30a8bd1
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-11-18 01:33:47 -0600

    Revert "cpufreq interactive governor: Unify attribute store functions"
    This reverts commit 55f60eca1c6f3e29c8de1e778fb248b978e15768.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -494,14 +494,9 @@ static ssize_t show_boost_factor(struct kobject *kobj,
 static ssize_t store_boost_factor(struct kobject *kobj,
 			struct attribute *attr, const char *buf, size_t count)
 {
-	int ret;
-	unsigned long val;
-
-	ret = strict_strtoul(buf, 0, &val);
-	if (ret < 0)
-		return ret;
-	boost_factor = val;
-	return count;
+	if (!strict_strtoul(buf, 0, &boost_factor))
+		return count;
+	return -EINVAL;
 }
 
 static struct global_attr boost_factor_attr = __ATTR(boost_factor, 0644,
@@ -516,14 +511,9 @@ static ssize_t show_max_boost(struct kobject *kobj,
 static ssize_t store_max_boost(struct kobject *kobj,
 			struct attribute *attr, const char *buf, size_t count)
 {
-	int ret;
-	unsigned long val;
-
-	ret = strict_strtoul(buf, 0, &val);
-	if (ret < 0)
-		return ret;
-	max_boost = val;
-	return count;
+	if (!strict_strtoul(buf, 0, &max_boost))
+		return count;
+	return -EINVAL;
 }
 
 static struct global_attr max_boost_attr = __ATTR(max_boost, 0644,
@@ -539,14 +529,9 @@ static ssize_t show_sustain_load(struct kobject *kobj,
 static ssize_t store_sustain_load(struct kobject *kobj,
 			struct attribute *attr, const char *buf, size_t count)
 {
-	int ret;
-	unsigned long val;
-
-	ret = strict_strtoul(buf, 0, &val);
-	if (ret < 0)
-		return ret;
-	sustain_load = val;
-	return count;
+	if (!strict_strtoul(buf, 0, &sustain_load))
+		return count;
+	return -EINVAL;
 }
 
 static struct global_attr sustain_load_attr = __ATTR(sustain_load, 0644,
