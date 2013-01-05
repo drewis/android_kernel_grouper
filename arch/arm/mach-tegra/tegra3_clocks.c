157ef8d604707462525a9575b9dc3edc0c83debb
Metallice <BryanUofD@gmail.com>
2013-01-04 23:48:38 -0500

    mach-tegra: tegra3_clocks.c: use max emc rate at LP max for smoothness

--- arch/arm/mach-tegra/tegra3_clocks.c
+++ arch/arm/mach-tegra/tegra3_clocks.c
@@ -4809,7 +4809,7 @@ unsigned long tegra_emc_to_cpu_ratio(unsigned long cpu_rate)
 
 	/* Vote on memory bus frequency based on cpu frequency;
 	   cpu rate is in kHz, emc rate is in Hz */
-	if (cpu_rate >= 925000)
+	if (cpu_rate >= 650000)
 		return emc_max_rate;	/* cpu >= 925 MHz, emc max */
 	else if (cpu_rate >= 450000)
 		return emc_max_rate/2;	/* cpu >= 450 MHz, emc max/2 */
