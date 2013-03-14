35135e6018935bfc8f3c374006d7aa520853228f
Metallice <BryanUofD@gmail.com>
2013-03-14 15:45:55 -0400

    include: linux: blkdev.h: increase max nr_requests to 512

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -33,7 +33,7 @@ struct sg_io_hdr;
 struct bsg_job;
 
 #define BLKDEV_MIN_RQ	4
-#define BLKDEV_MAX_RQ	128	/* Default maximum */
+#define BLKDEV_MAX_RQ	512	/* Non-Default maximum (128) */
 
 struct request;
 typedef void (rq_end_io_fn)(struct request *, int);
